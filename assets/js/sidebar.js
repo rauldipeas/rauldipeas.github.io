4571b9bc88836800a53cc741684cf10b7e205b4c
rauldipeas <rauldipeas@users.noreply.github.com>
2025-08-20 15:48:37 +0000

    deploy: d309335e583581e55c758682b229014ad579dfba

---
+++ assets/js/sidebar.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("MastodonSidebar").style.width = "350px";
+  }
+  
+  function closeNav() {
+    document.getElementById("MastodonSidebar").style.width = "0";
+  }
